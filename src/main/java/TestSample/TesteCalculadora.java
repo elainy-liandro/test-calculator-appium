0973f1b55b348160a8993ba58f7039951835e89c
Elainy Rodrigues <elainyr.s@gmail.com>
2023-06-24 12:29:35 -0300

    commit inicial

---
+++ src/main/java/TestSample/TesteCalculadora.java
@@ -0,0 +1,35 @@
+package TestSample;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class TesteCalculadora {
+
+    private AppiumDriver driver;
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+
+    public void iniciar() throws MalformedURLException {
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"emulator");
+        capabilities.setCapability(MobileCapabilityType.UDID,"emulator-5554");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        capabilities.setCapability("appPackage","com.google.android.calculator");
+        capabilities.setCapability("appActivity","com.android.calculator2.Calculator");
+
+        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/"), capabilities);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void TestCalculadora_Somar() throws MalformedURLException {
+        iniciar();
+
+    }
+
+}
